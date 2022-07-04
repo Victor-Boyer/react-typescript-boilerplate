695215a970238dbe7244c38df0f5cadeb9a578c1
Victor <victorboyer33@gmail.com>
2022-07-04 23:37:37 +0200

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,13 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+    <div className="bg-gradient-to-r from-cyan-500 to-blue-500 h-screen">
+      <header className="flex justify-center items-center h-full w-full">
+        <p className="text-2xl text-white font-semibold">
+          Fresh new boilterplate with TypeScript, React, and TailwindCSS.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
